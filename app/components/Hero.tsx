0733e72657fffd8e6b45b50f27a9193c394dd285
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-03-06 01:27:22 -0800

    update hero section with phone mockup

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 
 const Hero: React.FC = (): JSX.Element => (
-  <div className="hero min-h-screen">
-    <div className="hero-overlay bg-opacity-60"></div>
-    <div className="hero-content text-neutral-content text-center">
+  <div className="hero bg-base-200 min-h-screen">
+    <div className="hero-content flex-col lg:flex-row-reverse"></div>
+    <div className="hero-content text-neutral-content ">
       <div className="max-w-md">
-        <h1 className="mb-5 w-full text-5xl font-bold text-neutral-content">
-          AstroView
+        <h1 className="mb-5 w-full text-6xl md:text-7xl leading-tight font-bold text-neutral-content">
+          Astro
+          <span className="relative inline-block px-3 py-0.5 font-bold text-white bg-primary rounded-md transform rotate-[3deg]">
+            <div className="absolute inset-0 "></div>
+            <span className="relative inline-block transform -rotate-[3deg] ">
+              View
+            </span>
+          </span>
         </h1>
-        <p className="text-lg font-light text-neutral-content text-center mb-5">
-          <span className="font-bold">AstroView</span> is a simple web
+        <p className="text-lg font-light text-neutral-content text-center mb-5 ">
+          <span className="font-bold ">AstroView</span> is a simple web
           application made for The Star Gazer&apos;s club. It allows club
           members to search NASA&apos;s Astronomy Picture of the Day (APOD) by
           date.
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
+import Hero2 from "./components/Hero2";
 
 export default function Home() {
   return (
     <main>
-      <Hero />
+      <Hero2 />
     </main>
   );
 }
