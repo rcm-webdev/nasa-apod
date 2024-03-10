1258345e0353e1d7fb0ae566e37322e299ef88e2
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-03-09 17:54:31 -0800

    justify center main web application

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import Hero2 from "./components/Hero2";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex justify-center">
       <Hero2 />
     </main>
   );
