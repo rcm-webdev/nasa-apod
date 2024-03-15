1fd069449fdc9b85c6ba8aa62029856e30e19107
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-03-14 20:16:27 -0700

    add a features section, an accordion component

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import Hero from "./components/Hero";
 import Hero2 from "./components/Hero2";
 import Header from "./components/Header";
+import Features from "./components/Features";
 
 export default function Home() {
   return (
-    <main className="flex justify-center">
+    <main className="flex flex-col justify-center">
       <Hero2 />
+      <Features />
     </main>
   );
 }
