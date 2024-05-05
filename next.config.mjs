f9a2a88fc78bd1fd33b1a3de389c2306b691727f
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-05-04 18:05:22 -0700

    install axios

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "img.daisyui.com",
       },
+      {
+        protocol: "https",
+        hostname: "apod.nasa.gov",
+      },
+      {
+        protocol: "https",
+        hostname: "www.youtube.com",
+      },
     ],
   },
 };
